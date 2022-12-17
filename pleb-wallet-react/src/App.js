32b9e5546da99c3d14f307193ff4f87890d8ca7c
nickkrauts <nickkraut@gmail.com>
2022-12-17 06:38:36 +0000

    Initialize react app with pull from Coinbase API

--- pleb-wallet-react/src/App.js
+++ pleb-wallet-react/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [price, setPrice] = useState(0);
+  const getPrice = () => {
+    // Axios is a library that makes it easy to make http requests
+    // After we make a request, we can use the .then() method to handle the response asychronously
+    // This is an alternative to using async/await
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      // .then is a promise that will run when the API call is successful
+      .then((res) => {
+        console.log(res.data.data.amount);
+        setPrice(res.data.data.amount);
+       // updateChartData(res.data.data.amount);
+      })
+      // .catch is a promise that will run if the API call fails
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //Make API request fire every n seconds
+  useEffect(() => {
+    getPrice();
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>${price}</h1>
     </div>
   );
 }
